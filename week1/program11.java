0bd8c363097ba1c26beaec62ccf5033094d8c53d
Bhavya Vats <bhavya.vats05@gmail.com>
2023-01-24 20:04:14 +0530

    Constructor

--- week1/program11.java
+++ week1/program11.java
@@ -10,13 +10,15 @@ public class program11 {
     //PURPOSE
     //it creates/initializes objects in memory
     //we can use constructor block to initialise default values.
-    program11() {
-        empID=101;
-        salary=30000;
-        department="testing";
+    program11(int employeeID, int sal, String dep) {
+        empID=employeeID;
+        salary=sal;
+        department=dep;
     }
     public static void main(String[] args) {
-        program11 rahul=new program11();
+        program11 rahul=new program11(101,30000, "testing");
+        program11 varsha=new program11(101, 50000, "Engineering");
         System.out.println(rahul.empID +" " +  rahul.salary +" " +  rahul.department);
+        System.out.println(varsha.empID +" " +  varsha.salary +" " +  varsha.department);
     }
 }
