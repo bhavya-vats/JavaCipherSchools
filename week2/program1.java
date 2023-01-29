9a77d2b78903a96471b744705f80e94445803f4a
Bhavya Vats <108180977+bhavya-vats@users.noreply.github.com>
2023-01-29 12:27:09 +0530

    Update program1.java
    Signed-off-by: Bhavya Vats <108180977+bhavya-vats@users.noreply.github.com>

--- week2/program1.java
+++ week2/program1.java
@@ -1,1 +1,15 @@
+package week2;
+
+public class program15 {
+    int arg=5;
+    void myTest(int arg) { //default constructor
+        arg=arg;
+    }
+    public static void main(String[] args) {
+        int arg=10;
+        program15 obj=new program15();
+        obj.myTest(arg);
+        System.out.println(obj.arg);
+    }
+}
 
