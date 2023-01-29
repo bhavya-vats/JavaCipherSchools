5485ffa9489d0c09847aab5e7312add0073c5929
Bhavya Vats <108180977+bhavya-vats@users.noreply.github.com>
2023-01-29 13:58:19 +0530

    Update program2.java
    Signed-off-by: Bhavya Vats <108180977+bhavya-vats@users.noreply.github.com>

--- week2/program2.java
+++ week2/program2.java
@@ -10,7 +10,7 @@ class App {
 }
 public class program2 extends App{
     program2() {
-        super(); //used to call constructor of base class
+        super(20); //used to call constructor of base class
         System.out.println("Constructor of Test2");
     }
     public static void main(String[] args) {
